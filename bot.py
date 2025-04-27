a0499b98ed241e3391a3fd3a9024d2b32a899bc2
r.solikhov <r.solikhov@g.nsu.ru>
2025-04-27 19:11:59 +0300

    added: command help

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет, пришли свой текст")
+    bot.send_message(message.chat.id, "Привет, это бот проверяющий твои сочинения на соотвествие критериям ФИПИ")
+
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "Это раздел справочной информации")
 
 bot.polling(none_stop=True)
-
-
-
-
-
-
